762d09ba8626ec8c974ef05aff0aa74454c4e768
David Galdeano <galdeano@lirmm.fr>
2011-10-12 19:32:26 +0200

    Dox Signed-off-by: David Galdeano <galdeano@lirmm.fr>

--- DrawAllJoints.h
+++ DrawAllJoints.h
@@ -4,6 +4,25 @@
 #include <gsl/gsl_vector.h>
 #include <gsl/gsl_matrix.h>
 
+/** \file DrawAllJoints.h
+ *  \brief Draw the model of the robot
+ *  \author    David Galdeano
+ *  \date      10/2011
+ *
+ */
+
+
+/**
+ * \fn void DrawAllJoints(SuLINK uLINK[],int j)
+ * \brief Draw the model of the robot
+ *
+ * Must be used as follow:
+ * \code DrawAllJoints(uLINK,1); \endcode
+ *
+ *
+ * \param uLINK[] Structure wich describe the robot link by link
+ * \param j ID of the link
+ */
 void DrawAllJoints(SuLINK uLINK[],int j);
 
 
