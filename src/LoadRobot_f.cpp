7fa0d3435493901595e97e4b468220a61d57c7b4
gally_port_work <galdeano@lirmm.fr>
2026-06-10 20:30:48 +0200

    Replace ezxml parser with modern pugixml library

--- src/LoadRobot_f.cpp
+++ src/LoadRobot_f.cpp
@@ -9,20 +9,24 @@
 #include "ForwardKinematics_f.h"
 #include "LoadRobot_f.h"
 
-#include "./ezxml/ezxml.h"
+#include <pugixml.hpp>
 
 
 
 void LoadRobotParserXML_f(Struct_uLINK uLINK[],Struct_State *Status,char* RobotFile)
 {
-
     int i;
     int dof,numlink;
 
-    ezxml_t robot = (ezxml_t)ezxml_parse_file(RobotFile), State, Link;
+    pugi::xml_document doc;
+    if (!doc.load_file(RobotFile))
+    {
+        perror("Error opening robot description file");
+        return;
+    }
+    pugi::xml_node robot = doc.child("Robot");
 
-    sscanf (ezxml_child(robot, "DoF")->txt,"%d",&dof);
-    //printf("dof: %d \n", dof);
+    dof = robot.child("DoF").text().as_int();
 
     for(i=1; i<(dof+2); i++)
     {
@@ -41,33 +45,30 @@ void LoadRobotParserXML_f(Struct_uLINK uLINK[],Struct_State *Status,char* RobotF
         MatClrf(uLINK[i].hv,3,1);
     }
 
-
     Status->ddl=dof+6;
     Status->support=0; //0:none,1:right,2:left,3:both
     Status->desired_support=0;
     Status->distribution_y=0.5;
 
-    State = ezxml_child(robot, "State");
-    int temp_right_foot_ID, temp_left_foot_ID;
-    sscanf (ezxml_child(State, "right_foot_ID")->txt,"%d",&temp_right_foot_ID);
+    pugi::xml_node State = robot.child("State");
+    int temp_right_foot_ID = State.child("right_foot_ID").text().as_int();
     Status->right_foot_ID = (unsigned char)temp_right_foot_ID;
-    sscanf (ezxml_child(State, "left_foot_ID")->txt,"%d",&temp_left_foot_ID);
+    int temp_left_foot_ID = State.child("left_foot_ID").text().as_int();
     Status->left_foot_ID = (unsigned char)temp_left_foot_ID;
 
-    for (Link = ezxml_child(robot, "Link"); Link; Link = Link->next)
+    for (pugi::xml_node Link : robot.children("Link"))
     {
-        numlink = atoi(ezxml_attr(Link, "no"));
-        sscanf (ezxml_child(Link, "Name")->txt,"%s", &uLINK[numlink].name);
-        sscanf (ezxml_child(Link, "m")->txt,"%lf", &uLINK[numlink].m);
-        int temp_sister, temp_child;
-        sscanf (ezxml_child(Link, "sister")->txt,"%d", &temp_sister);
+        numlink = Link.attribute("no").as_int();
+        strcpy(uLINK[numlink].name, Link.child("Name").text().as_string());
+        uLINK[numlink].m = Link.child("m").text().as_double();
+        int temp_sister = Link.child("sister").text().as_int();
         uLINK[numlink].sister = (unsigned char)temp_sister;
-        sscanf (ezxml_child(Link, "child")->txt,"%d", &temp_child);
+        int temp_child = Link.child("child").text().as_int();
         uLINK[numlink].child = (unsigned char)temp_child;
-        sscanf (ezxml_child(Link, "a")->txt,"%lf %lf %lf", &uLINK[numlink].a[0], &uLINK[numlink].a[1], &uLINK[numlink].a[2]);
-        sscanf (ezxml_child(Link, "b")->txt,"%lf %lf %lf", &uLINK[numlink].b[0], &uLINK[numlink].b[1], &uLINK[numlink].b[2]);
-        sscanf (ezxml_child(Link, "p")->txt,"%lf %lf %lf", &uLINK[numlink].p[0], &uLINK[numlink].p[1], &uLINK[numlink].p[2]);
-        sscanf (ezxml_child(Link, "c")->txt,"%lf %lf %lf", &uLINK[numlink].c[0], &uLINK[numlink].c[1], &uLINK[numlink].c[2]);
+        sscanf (Link.child("a").text().as_string(), "%lf %lf %lf", &uLINK[numlink].a[0], &uLINK[numlink].a[1], &uLINK[numlink].a[2]);
+        sscanf (Link.child("b").text().as_string(), "%lf %lf %lf", &uLINK[numlink].b[0], &uLINK[numlink].b[1], &uLINK[numlink].b[2]);
+        sscanf (Link.child("p").text().as_string(), "%lf %lf %lf", &uLINK[numlink].p[0], &uLINK[numlink].p[1], &uLINK[numlink].p[2]);
+        sscanf (Link.child("c").text().as_string(), "%lf %lf %lf", &uLINK[numlink].c[0], &uLINK[numlink].c[1], &uLINK[numlink].c[2]);
     }
 
     FindMother_f(uLINK,1);
